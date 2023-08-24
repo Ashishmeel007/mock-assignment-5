09a38595cfb09461004a7ef08d3968bab5764802
Ashish meel <ashishmeel22@gmail.com>
2023-08-24 20:34:56 +0530

    done

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
  require('dotenv').config();
  
-const connection = mongoose.connect(process.env.mongoURL);
+ const connection = mongoose.connect(process.env.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 module.exports = {
     connection
